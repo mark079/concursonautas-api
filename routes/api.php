91ca386df48fb14fbb38422f01230a684dca9ec8
Mark079 <joaopedromaximo@gmail.com>
2023-11-08 08:20:13 -0300

    Configured routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Controllers\V1\GoalController;
+use App\Http\Controllers\V1\ScheduleController;
+use App\Http\Controllers\V1\StudyBlockController;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/users', UserController::class);
+    Route::apiResource('/goals', GoalController::class);
+    Route::apiResource('/schedules', ScheduleController::class);
+    Route::apiResource('/study_blocks', StudyBlockController::class);
 });
